53a97f8cc9bbbd6a35d220fff6a01ffa99f6ef30
Waraporn Khamla <warapornkhamla@Waraporns-Air.fritz.box>
2024-05-17 15:00:28 +0200

    new about component

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,15 +1,30 @@
 
+import { Route, Routes } from 'react-router-dom';
+
+import App from './App.jsx';
+import About from './components/About.jsx';
 
 const Router =()=>{
 
     return(
-    <>
-    <h1>Router</h1>
     
-    </>
+  
+    <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/about" element={<About/>} />
+            
+
+
+
+
+        </Routes>
+    
+    
     
 )
 
 }
 
 export default Router
+
+
